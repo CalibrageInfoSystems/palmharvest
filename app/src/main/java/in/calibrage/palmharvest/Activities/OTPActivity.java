3cb79d58eeaf0b8e603f8e8a3ae9058e818f5e68
PrasadMusini-Dev <durgaprasad.musini@calibrage.in>
2025-05-09 17:37:37 +0530

    Commiting Otp Activity

--- app/src/main/java/in/calibrage/palmharvest/Activities/OTPActivity.java
+++ app/src/main/java/in/calibrage/palmharvest/Activities/OTPActivity.java
@@ -7,12 +7,15 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -43,12 +46,13 @@ public class OTPActivity extends BaseActivity {
     private Button sub_Btn;
     private String currentDate,Farmer_code;
     private PinEntryEditText pinEntry;
-    private ImageView backImg;
+//    private ImageView backImg;
     TextView otp_desc;
     String Reg_mobilenumber;
     private SpotsDialog mdilogue;
     String F_number,S_number;
     String    userOtp;
+    EditText otp1, otp2, otp3, otp4, otp5, otp6;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,21 +77,78 @@ public class OTPActivity extends BaseActivity {
 //                Settings.Secure.ANDROID_ID);
 //        Log.e("deviece==id", Device_id);
         sub_Btn = (Button) findViewById(R.id.btn_otp_login);
-        backImg = (ImageView) findViewById(R.id.back);
+//        backImg = (ImageView) findViewById(R.id.back);
         otp_desc =(TextView)findViewById(R.id.otp_desc);
-        pinEntry = findViewById(R.id.txt_pin_entry);
-        pinEntry.requestFocus();
+//        pinEntry = findViewById(R.id.txt_pin_entry);
+//        pinEntry.requestFocus();
         mdilogue = new SpotsDialog(this, R.style.Custom);
-//        mdilogue = (SpotsDialog) new SpotsDialog.Builder()
-//                .setContext(this)
-//                .setTheme(R.style.Custom)
-//                .build();
 
+        // Get intent extras
+//        Intent intent = getIntent();
+//        String mobileNumber = intent.getStringExtra("mobile");
+//        otp_title.setText("Enter the 6 Digits Code Sent your Registered Mobile Number(s) " + mobileNumber);
+
+        otp1 = findViewById(R.id.otp1);
+        otp2 = findViewById(R.id.otp2);
+        otp3 = findViewById(R.id.otp3);
+        otp4 = findViewById(R.id.otp4);
+        otp5 = findViewById(R.id.otp5);
+        otp6 = findViewById(R.id.otp6);
+
+        setOtpFocusListeners();
 
         SharedPreferences pref = getSharedPreferences("FARMER", MODE_PRIVATE);
 
         userOtp = pref.getString("userName", "");
 
+    }
+
+
+    private void setOtpFocusListeners() {
+        otp1.addTextChangedListener(new SimpleTextWatcher(otp2));
+        otp2.addTextChangedListener(new SimpleTextWatcher(otp3));
+        otp3.addTextChangedListener(new SimpleTextWatcher(otp4));
+        otp4.addTextChangedListener(new SimpleTextWatcher(otp5));
+        otp5.addTextChangedListener(new SimpleTextWatcher(otp6));
+
+        setBackspaceHandler(otp2,otp1);
+        setBackspaceHandler(otp3,otp2);
+        setBackspaceHandler(otp4,otp3);
+        setBackspaceHandler(otp5,otp4);
+        setBackspaceHandler(otp6,otp5);
+    }
+
+//    private void setBackspaceHandler(EditText current, EditText previous) {
+//        current.setOnKeyListener((v, keyCode, event) -> {
+//            if (event.getAction() == KeyEvent.ACTION_DOWN &&
+//                    keyCode == KeyEvent.KEYCODE_DEL &&
+//                    current.getText().toString().isEmpty()) {
+//                previous.requestFocus();
+//                previous.setText("");
+//                return true;
+//            }
+//            return false;
+//        });
+//    }
+private void setBackspaceHandler(final EditText current, final EditText previous) {
+    current.setOnKeyListener(new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_DEL &&
+                    current.getText().toString().isEmpty()) {
+                previous.requestFocus();
+                previous.setText("");
+                return true;
+            }
+            return false;
+        }
+    });
+}
+
+    private String getOtpCode() {
+        return otp1.getText().toString() + otp2.getText().toString() + otp3.getText().toString() +
+                otp4.getText().toString() + otp5.getText().toString() + otp6.getText().toString();
     }
 
 
@@ -106,34 +167,54 @@ public class OTPActivity extends BaseActivity {
                 otp_desc.setText(getString(R.string.otp_desc) + " " + number);
             }
         }
+//        sub_Btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (pinEntry.getText() != null & pinEntry.getText().toString().trim() != "" & !TextUtils.isEmpty(pinEntry.getText())) {
+//                    if (isOnline())
+//                        GetUserOtp();
+//
+//                    else {
+//                        showDialog(OTPActivity.this, getResources().getString(R.string.Internet));
+//                    }
+//                } else {
+//                    showDialog(OTPActivity.this, getResources().getString(R.string.ente_pin));
+//
+//                }
+//            }
+//        });
         sub_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (pinEntry.getText() != null & pinEntry.getText().toString().trim() != "" & !TextUtils.isEmpty(pinEntry.getText())) {
-                    if (isOnline())
-                        GetUserOtp();
 
+                String otpCode = getOtpCode();
+                if (TextUtils.isEmpty(otpCode)) {
+                    showDialog(OTPActivity.this, getResources().getString(R.string.ente_pin));
+                } else if (otpCode.length() < 6) {
+                    showDialog(OTPActivity.this, "Please enter a valid OTP.");
+                } else {
+                    if (isOnline()) {
+                        GetUserOtp(otpCode);
+                    }
                     else {
                         showDialog(OTPActivity.this, getResources().getString(R.string.Internet));
                     }
-                } else {
-                    showDialog(OTPActivity.this, getResources().getString(R.string.ente_pin));
-
                 }
             }
         });
-        backImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+
+//        backImg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//            }
+//        });
     }
 
-    private void GetUserOtp() {
+    private void GetUserOtp(String otpCode) {
         mdilogue.show();
         ApiService service = ServiceFactory.createRetrofitService(this, ApiService.class);
-        mSubscription = service.getUserOTPResponsePage(APIConstantURL.get_userOTP + userOtp + "/" + pinEntry.getText().toString())
+        mSubscription = service.getUserOTPResponsePage(APIConstantURL.get_userOTP + userOtp + "/" + otpCode)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<GetUserOTPResponse>() {
                     @Override
@@ -198,4 +279,27 @@ if(getUserOTPResponse.getResult().getRoleId() ==2)
 
     }
 
+
+    private class SimpleTextWatcher implements android.text.TextWatcher {
+        private final EditText nextEditText;
+
+        public SimpleTextWatcher(EditText nextEditText) {
+            this.nextEditText = nextEditText;
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (editable.length() > 0 && nextEditText != null) {
+                nextEditText.requestFocus();
+            }
+        }
+
+    }
 }
